32cd5419749b66162022e660a122d9e33a3c3fce
paisley <paisley.chien@mwhglobal.com>
2016-04-17 00:42:33 +0800

    新增DetailsViewModels及處理下方清單畫面

--- MoneyAsset/Controllers/HomeController.cs
+++ MoneyAsset/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MoneyAsset.Models;
+using MoneyAsset.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +27,23 @@ namespace MoneyAsset.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        [ChildActionOnly]
+        public ActionResult Details()
+        {
+            var results = GetMoneyData();
+            return View(results);
+        }
+
+        private static List<DetailsViewModels> GetMoneyData()
+        {
+           List<DetailsViewModels> results = new List<DetailsViewModels> {
+               new DetailsViewModels {Id=1, Categories="支出", Cost=300, CreateDate=new DateTime(2016,1,3) },
+               new DetailsViewModels {Id=2, Categories="支出", Cost=1600,CreateDate=new DateTime(2016,3,10) },
+               new DetailsViewModels {Id=3, Categories="支出", Cost=800, CreateDate=new DateTime(2016,4,1) }
+           };
+
+            return results;
         }
     }
 }
---
+++ MoneyAsset/Models/ViewModels/DetailsViewModels.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoneyAsset.Models.ViewModels
+{
+    public class DetailsViewModels
+    {
+        public int Id { get; set; }
+        public string Categories { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int Cost { get; set; }
+        public string Remark { get; set; }
+    }
+}
