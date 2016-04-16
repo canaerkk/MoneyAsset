1e7b9b6df5a978f83cac223b35755be1179c239f
paisley <paisley.chien@mwhglobal.com>
2016-04-16 23:30:47 +0800

    First Commit

---
+++ MoneyAsset/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoneyAsset.MvcApplication" Language="C#" %>
---
+++ MoneyAsset/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoneyAsset.Startup))]
+namespace MoneyAsset
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
